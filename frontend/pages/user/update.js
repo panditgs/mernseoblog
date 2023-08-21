2e62cdb3e4bc3b24607f1ed33d6e3770d250238e
pandith3030 <pandit385@gmail.com>
2023-08-21 20:09:51 +0530

    Initial commit

---
+++ frontend/pages/user/update.js
@@ -0,0 +1,22 @@
+import Layout from '../../components/Layout';
+import React from "react";
+
+import Private from '../../components/auth/Private';
+import ProfileUpdate from '../../components/auth/ProfileUpdate';
+import Link from 'next/link';
+
+const UserProfileUpdate = () => {
+    return (
+        <Layout>
+            <Private>
+                <div className="container-fluid">
+                    <div className="row">
+                        <ProfileUpdate />
+                    </div>
+                </div>
+            </Private>
+        </Layout>
+    );
+};
+
+export default UserProfileUpdate;
